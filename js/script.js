ea855a300217f4196f4c4353b133411f27b51d08
Agustin Marseillan <amarseillan@Agustins-MacBook-Pro.local>
2018-10-16 19:44:03 -0300

    creacion de lineas

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
 var itemspaces = [];
 var $espacioLista;
+var botonFunctions = false;
 
 $(document).ready( function(){
 	$espacioLista = $('#items');
-	$('#botonItem').addEventListener('click', newItem);
+	$('#botonItem').click(addNewItem);
+	//$('#nuevaLista').click(newCompleteList);
 });
 
-function Item() {
-	this.check = '<img alt="No completada" src="img/cross.png"';
-	this.texto = prompt('Escriba el elemento que quiere agregar a la lista.');
-	this.button1 = '<button>Editar</button>';
-	this.button2 = '<button>Quitar</button>';
+function addNewItem(){
+	var input = prompt('Nuevo elemento de la lista:');
+	$espacioLista.append('<li><img src="img/cross.png" alt="not done"><h4>'+input+'</h4><button class="modify">Cambiar</button><button class="delete">Quitar</button></li>');
+	$('.modify').click(modify);
+	$('.delete').click(deleteIt);
 }
 
-function newItem() {
-	var itemLocal = new Item();
-	$itemspaces.push('a');
-	var pos = $itemspaces.indexOf('a');
-	$espacioLista.innerHTML += '\n<li id="item'pos'">'+itemLocal.check+itemLocal.texto+itemLocal.button1+itemLocal.button2+'</li>';
-	$itemspaces[pos] = $('#item'+pos);
-}
+
+function modify(input) {
+	var input = prompt('Ingrese el nuevo contenido:');
+	$(this).parent().children('h4').html(input);
+};
+function deleteIt() {
+	$(this).parent().remove();
+};
+
