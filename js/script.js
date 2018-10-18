a81a3f7c65222b0f031b9f5e2e5af8aecb89a899
Agustin Marseillan <amarseillan@Agustins-MacBook-Pro.local>
2018-10-18 14:14:53 -0300

    funciones sprint 1 completas(add/modify/delete/toggle lines)

--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
 var itemspaces = [];
-var $espacioLista;
+var $listaIncompletos;
+var $listaCompletos;
 var botonFunctions = false;
 
 $(document).ready( function(){
-	$espacioLista = $('#items');
+	$listaIncompletos = $('#itemsIncompletos');
+	$listaCompletos = $('#itemsCompletos');
 	$('#botonItem').click(addNewItem);
 	//$('#nuevaLista').click(newCompleteList);
+	$listaIncompletos.on('click', 'li .modify', modify);
+	$listaIncompletos.on('click', 'li .delete', deleteIt);
+	$('#lista').on('click', 'li img', toggleCompletionState);
 });
 
 function addNewItem(){
 	var input = prompt('Nuevo elemento de la lista:');
-	$espacioLista.append('<li><img src="img/cross.png" alt="not done"><h4>'+input+'</h4><button class="modify">Cambiar</button><button class="delete">Quitar</button></li>');
-	$('.modify').click(modify);
-	$('.delete').click(deleteIt);
+	$listaIncompletos.append('<li><img src="img/cross.png" alt="not done"><h4>'+input+'</h4><button class="modify">Cambiar</button><button class="delete">Quitar</button></li>');
 }
 
 
-function modify(input) {
+function modify() {
 	var input = prompt('Ingrese el nuevo contenido:');
 	$(this).parent().children('h4').html(input);
 };
 function deleteIt() {
 	$(this).parent().remove();
 };
+function toggleCompletionState() {
+	if ($(this).attr('alt') == 'not done'){
+		$(this).attr({'src':'img/check.png','alt':'done'});	
+		$(this).parent().appendTo($listaCompletos);
+	} else {
+		$(this).attr({'src':'img/cross.png','alt':'not done'});
+		$(this).parent().appendTo($listaIncompletos);
+	}
+}
 
